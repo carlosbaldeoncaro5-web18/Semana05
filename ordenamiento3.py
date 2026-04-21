87338ae046628c265afb502e988bdbcfae5aea88
carlosbaldeoncaro5-web18 <carlosbaldeoncaro5@gmail.com>
2026-04-20 20:18:26 -0500

    ord burbuja

---
+++ ordenamiento3.py
@@ -0,0 +1,3 @@
+cubo = [2,8,5,3,9,4,1 ]
+for i in range (len(cubo)):
+    for 
--- ordenamientos.py
+++ ordenamientos.py
@@ -1,3 +1,13 @@
+def ordburbuja(lst):
+    
+    n= len(lst)
+    for pasada in range (n-1):
+        for izqpar in range(n -1 - pasada):
+            derpar = izqpar+1
+            if lst[izqpar] > lst[derpar]:
+                lst[izqpar], lst[derpar] = lst[derpar], lst[izqpar]
+
+
 def ordseleccion(lst):
     n= len(lst)
     for manoizq in range (n-1):
@@ -6,7 +16,7 @@ def ordseleccion(lst):
             if lst [posMen] > lst[ver]:
                 posMen = ver 
         lst [ manoizq], lst[posMen] = lst[posMen], lst[manoizq]
-        
+
 
 
 list = [2,8,5,3,9,4,1]
