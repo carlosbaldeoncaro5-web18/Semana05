446bb7bd503ebc988d4107d3684704dfd3480950
carlosbaldeoncaro5-web18 <carlosbaldeoncaro5@gmail.com>
2026-04-20 20:25:35 -0500

    ord burbuja

--- ordenamiento2.py
+++ ordenamiento2.py
@@ -1,4 +1,15 @@
+def ordernar_seleccion (lista): 
+    n = len(lista) 
+    for j in range (n-1): 
+        imin = j 
+        for i in range (j + 1, n): 
+            if lista[i] < lista[imin]: 
+                imin = i 
+        if imin != j: 
+            lista[j], lista[imin] = lista[imin], lista[j] 
 
 
-
-def order
+mi_lista = [2, 8, 5, 3, 9, 4, 1] 
+print("Sin ordenar:", mi_lista) 
+ordernar_seleccion(mi_lista) 
+print("Ordenada:", mi_lista)
--- ordenamiento3.py
+++ ordenamiento3.py
@@ -1,3 +1,9 @@
-cubo = [2,8,5,3,9,4,1 ]
-for i in range (len(cubo)):
-    for 
+cubo = [2,8,5,3,9,4,1] 
+for i in range (len(cubo)): 
+    for j in range(0, len(cubo) -i -1): 
+        if cubo[j] > cubo[j + 1]: 
+            cb = cubo[j] 
+            cubo[j] = cubo[j+1] 
+            cubo[j+1] = cb 
+
+print (cubo)
